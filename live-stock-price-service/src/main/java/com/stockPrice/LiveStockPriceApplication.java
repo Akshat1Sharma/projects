09d656ef1b85ac9af077cd2dab576174c98183e8
Akshat <49338148+Akshat1Sharma@users.noreply.github.com>
2025-04-04 16:29:13 +0530

    live stock price service project
    live stock price service project

---
+++ live-stock-price-service/src/main/java/com/stockPrice/LiveStockPriceApplication.java
@@ -0,0 +1,13 @@
+package com.stockPrice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveStockPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveStockPriceApplication.class, args);
+	}
+
+}
---
+++ live-stock-price-service/src/test/java/com/stockPrice/LiveStockPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockPrice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveStockPriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
